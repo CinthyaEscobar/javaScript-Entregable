c854ecc23c0a84ed5a55cb4682580b7e7f70efb8
Cinthya Escobar <cinthyacarolinaescobar@gmail.com>
2022-08-24 16:42:41 -0400

    actividad complementaria

---
+++ miArchivo.js
@@ -0,0 +1,25 @@
+/* let numero = prompt('Ingrese un numero')
+for(let i = 1; i <= numero; i++){
+        alert(i)
+} */
+
+/* alert('Tabla multiplicar')
+let numero = prompt('ingrese un numero')
+
+for(let i = 1; i<=10;i++){
+    resultado= numero*i
+    alert(`${numero} X  ${i} =  ${resultado}`)
+}  */
+
+alert('Solo para mayores de edad')
+let edad = prompt('Ingrese su edad')
+while(edad<18){
+    edad = prompt('Edad insuficiente. Vuelva a escribir su edad')
+} 
+alert('Mayor de edad, puede pasar')
+
+/* let edad2;
+do{
+    edad2 = prompt('Ingrese su edad')
+}while(edad2<18);
+alert('Mayor de edad, puede pasar') */
